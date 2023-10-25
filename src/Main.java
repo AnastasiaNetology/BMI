98d6ad6cd062c20f0341923986f4eecb72190e9f
Anastasia <barabanova.aaa@gmail.com>
2023-10-25 09:58:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double height = 1.87;
+        int weight = 98;
+        double bmi = service.calculate(height, weight); //должно получиться 28
+        System.out.println();
+        System.out.println("Индекс массы тела:");
+        System.out.println(bmi);
+
+        System.out.println("ИМТ:");
+        System.out.println(service.calculate(1.53,51));
+    }
+}
